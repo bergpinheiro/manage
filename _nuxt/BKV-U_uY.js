9360730817eb3fe80f47f681254145217b99c972
Berg Pinheiro <berg.pinheiro2009@gmail.com>
2026-06-24 22:32:27 -0300

    deploy: fix worklet path — load from /dashboard/ base URL

---
+++ _nuxt/BKV-U_uY.js
@@ -0,0 +1,1 @@
+import r from"./BIVVybRN.js";import{t,S as o}from"./kM-t5DrC.js";import"./Nw1fYS0Q.js";import"./ClZCl_fn.js";const i={__name:"AppSidebar",setup(e){return(p,a)=>(t(),o(r))}};export{i as default};
---
+++ _nuxt/Bib9kPF5.js
@@ -0,0 +1,1 @@
+import{ar as r}from"./kM-t5DrC.js";var e=r();export{e as O};
